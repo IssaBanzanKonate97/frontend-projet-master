2a6d7b8061fea9296099587bacb601214d4c57c0
issa <issabanzan.konate@yahoo.fr>
2024-02-18 01:56:51 +0100

    push 18/02/2024

--- src/app_components/PractitionerDetails/components/PaiementComponent.jsx
+++ src/app_components/PractitionerDetails/components/PaiementComponent.jsx
@@ -12,25 +12,30 @@ const PaiementComponent = () => {
       formData.append('last_name', 'Doe');
       formData.append('client_email', 'john.doe@swikly.com');
       formData.append('swik_description', 'Test Swikly API');
-      formData.append('swik_amount', 100);
+      formData.append('swik_amount', '100');
       formData.append('swik_type', 'deposit');
-      formData.append('id', 'MY_BUSINESS_ID');
-      formData.append('swik_end_day', 28);
-      formData.append('swik_end_month', 2);
-      formData.append('swik_end_year', 2019);
-      formData.append('email', true);
-      formData.append('sms', false);
+      //formData.append('id', 'MY_BUSINESS_ID');
+      //'Missing parameters: client_email, swik_amount, swik_lang, swik_description, swik_type'
+      formData.append('swik_end_day', '1');
+      formData.append('swik_end_month', '3');
+      formData.append('swik_end_year', '2024');
+      formData.append('email', 'true');
+      formData.append('sms', 'true');
       formData.append('swik_lang', 'FR');
       formData.append('phone_number', '+33601020304');
+      formData.append('swik_start_day', '18');
+      formData.append('swik_start_month', '2');
+      formData.append('swik_start_year', '2024');
 
-      const response = await axios.post('http://api/proxy-swikly/api/v1_0/newSwik', formData, {
+      const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/v1_0/newSwik`, formData, {
         headers: {
-
           'Content-Type': 'multipart/form-data'
         }
       });
 
-      console.log(response.data);
+      response.data && setPaymentUrl(response.data.acceptUrl);
+      console.log('Paiement Swikly créé :', response.data);
+
     } catch (error) {
       console.error('Erreur lors de la création du paiement Swikly :', error);
     }
